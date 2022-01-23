3a7c225470cbf3f1166e4688d2cc6b119e19655c
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-01-23 10:15:44 +0100

    added intro of arrays basics

---
+++ arrays/Introduction/array-intro.js
@@ -0,0 +1,21 @@
+const strings = ["a", "b", "c", "d"];
+
+// To get access to values (This occur in constant time)
+const getSecondElement = strings[2]; // O(1)
+console.log(getSecondElement);
+
+// push: To push an element into an array
+strings.push("e"); // O(1)
+console.log(strings);
+
+// pop: To remove the last item in the array
+strings.pop(); //O(1)
+console.log(strings);
+
+// unshift: To add an element to the beginning of the array
+strings.unshift("x");
+console.log(strings);
+
+// splice:  To add element within an array
+strings.splice(2, 0, "alien");
+console.log(strings);
---
+++ big-o/array-basic.js
@@ -0,0 +1,2 @@
+const array1 = ["a", "b", "c", "d"];
+const array2 = ["z", "y", "a"];
