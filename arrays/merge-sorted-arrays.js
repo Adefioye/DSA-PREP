ea269fba398398cf429cb480f35a6e1daa3134d3
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-01-25 02:52:11 +0100

    finished merge sorted arrays

--- arrays/merge-sorted-arrays.js
+++ arrays/merge-sorted-arrays.js
@@ -21,4 +21,41 @@ function mergeSortedArrays(arr1, arr2) {
   return mergedArray.sort((a, b) => a - b);
 }
 
-console.log(mergeSortedArrays(array1, array2));
+// console.log(mergeSortedArrays(array1, array2));
+
+function mergeSortedArrays2(array1, array2) {
+  // Typical assumption is that array is sorted
+  // check inputs to confirm they have elements, if one of then don't, return the other
+  const mergeArrays = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 0;
+  let j = 0;
+
+  if (array1.length === 0) {
+    return arr2.sort();
+  }
+
+  if (array2.length === 0) {
+    return arr1.sort();
+  }
+
+  if (Array.isArray(array1) !== true || Array.isArray(array2) !== true) {
+    return "Incorrect input, Please use an array object";
+  }
+
+  while (array1Item || array2Item) {
+    if (!array2Item || array1Item < array2Item) {
+      console.log(array1Item, array2Item);
+      mergeArrays.push(array1Item);
+      i++;
+      array1Item = array1[i];
+    } else {
+      mergeArrays.push(array2Item);
+      j++;
+      array2Item = array2[j];
+    }
+  }
+  return mergeArrays;
+}
+console.log(mergeSortedArrays2([0, 3, 4, 31], [4, 6, 30]));
