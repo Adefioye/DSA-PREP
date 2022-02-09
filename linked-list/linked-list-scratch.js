ae167fc40b97be62ebedb8806c16441cde24af11
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-02-09 21:42:30 +0100

    finished implementation of singly linked list from scratch

--- linked-list/linked-list-scratch.js
+++ linked-list/linked-list-scratch.js
@@ -1,6 +1,7 @@
 // Create first linked list from scratch
 // 10-->5-->16
 
+// MY IMPLEMENTATION (SOLUTION 1)
 class Node {
   constructor(value) {
     this.value = value;
@@ -17,6 +18,7 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
   printList() {
     const array = [];
     let currentNode = this.head;
@@ -26,15 +28,16 @@ class LinkedList {
     }
     return array;
   }
+
   append(value) {
-    const newNode = new Node(value);
+    const newNode = { value: value, next: null };
     this.tail.next = newNode;
     this.tail = newNode;
-    this.length++;
+    this.length += 1;
     return this;
   }
   prepend(value) {
-    const newNode = new Node(value);
+    const newNode = { value: value, next: null };
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
@@ -52,19 +55,58 @@ class LinkedList {
       return this.printList();
     }
 
-    const newNode = new Node(value)
-    let counter = 0 
-    let leader = this.head
-    while ((counter + 1) < index) {
-      leader = leader.next 
-      counter++
+    const newNode = { value: value, next: null };
+    let counter = 0;
+    let leader = this.head;
+    while (counter + 1 < index) {
+      leader = leader.next;
+      counter++;
     }
-    newNode.next = leader.next 
-    leader.next = newNode 
+    newNode.next = leader.next;
+    leader.next = newNode;
+    this.length++;
+    console.log(this.length);
+    return this.printList();
+  }
 
-    return this.printList()
+  remove(index) {
+    // Conditional fpr removing first item
+    if (index === 0) {
+      const leader = this.head;
+      this.head = leader.next;
+      console.log(this.length);
+      this.length--;
+      console.log(this.length);
+
+      return this.printList();
+    }
+
+    // Conditional for removing last item
+    if (index + 1 === this.length) {
+      let leader = this.head;
+
+      for (let i = 0; i < this.length - 2; i++) {
+        leader = leader.next;
+      }
+      leader.next = null;
+      this.length--;
+      return this.printList();
+    }
+
+    let leader = this.head;
+    let counter = 0;
+    while (counter + 1 < index) {
+      leader = leader.next;
+      counter++;
+    }
+    let follower = leader.next.next;
+    leader.next = follower;
+    this.length--;
+    return this.printList();
   }
 }
+
+
 const myLinkedList = new LinkedList(10);
 console.log(myLinkedList);
 myLinkedList.append(5);
@@ -73,11 +115,14 @@ myLinkedList.append(16);
 console.log(myLinkedList.printList());
 myLinkedList.prepend(7);
 console.log(myLinkedList.printList());
-console.log(myLinkedList.insert(10, 99))
-console.log(myLinkedList.insert(0, 25))
-console.log(myLinkedList.insert(1, 55))
-console.log(myLinkedList.insert(3, 56))
-console.log(myLinkedList.insert(5, 57))
+console.log(myLinkedList.insert(10, 99));
+console.log(myLinkedList.insert(0, 25));
+console.log(myLinkedList.insert(1, 55));
+console.log(myLinkedList.insert(3, 56));
+console.log(myLinkedList.insert(5, 57));
+console.log(myLinkedList.remove(0));
+console.log(myLinkedList.remove(7));
+console.log(myLinkedList.remove(4));
 
 // Experiment with the Node class
 // const myNode = new Node(10);
