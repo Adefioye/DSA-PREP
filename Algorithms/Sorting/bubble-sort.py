8e6b27fb1c321072c0b472f540f70d9ec2e2686f
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-04-25 23:16:11 +0100

    Implemented bubble sort algorithm

---
+++ Algorithms/Sorting/bubble-sort.py
@@ -0,0 +1,17 @@
+# 
+
+arr = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+def bubble_sort(arr):
+    length = len(arr)
+    for i in range(length - 1):
+        for j in range(length - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j + 1]
+                arr[j + 1] = arr[j]
+                arr[j] = temp 
+    
+    return arr 
+
+
+print(bubble_sort(arr))
