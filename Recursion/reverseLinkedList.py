1cceb1fab3ee524dbc9eccc750707e5071f67b52
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-02-08 17:08:26 -0600

    Solution to reverse linked list

---
+++ Recursion/reverseLinkedList.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next 
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.val, end=" ")
+            temp = temp.next
+        print()
+
+
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+
+def reverseLinkedList(head):
+
+    if head == None or head.next == None:
+        return head 
+
+    p = reverseLinkedList(head.next)
+
+    head.next.next = head 
+    head.next = None 
+
+    return p
+
+
+result = reverseLinkedList(head)
+result.print_list()
