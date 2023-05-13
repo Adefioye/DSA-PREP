8c1f13eecc27edf36deb4e4cc8362cf8c21a3dc4
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-05-13 02:46:32 -0500

    Added tab solution to canConstruct problem

---
+++ DynamicProgramming/canConstructFib.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+def canConstruct(target, wordBank):
+
+    tab = [False] * (len(target) + 1)
+    tab[0] = True
+
+    for i in range(len(tab)):
+
+        if tab[i] is True:
+
+            for word in wordBank:
+
+                if target[i : i + len(word)] == word:
+                    tab[i + len(word)] = True 
+
+    return tab[len(target)]
+
+
+
+
+
+
+print(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))                   # TRUE
+print(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))    # FALSE
+print(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))  # TRUE
+print(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee"
+])) # FALSE
