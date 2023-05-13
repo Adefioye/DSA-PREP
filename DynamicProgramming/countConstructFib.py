75a1b369f83b845a406f5710285b7ee85c0f8b16
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-05-13 04:01:18 -0500

    Added tab solution to countConstruct problem

---
+++ DynamicProgramming/countConstructFib.py
@@ -0,0 +1,26 @@
+
+# time = O(m*n*m) and space = O(m)
+def countConstruct(target, wordBank):
+    tab = [0] * (len(target) + 1)
+    tab[0] = 1
+
+    for i in range(len(tab)):
+        for word in wordBank:
+
+            if target[i : i + len(word)] == word:
+                tab[i + len(word)] += tab[i]
+
+    return tab[len(target)]
+
+print(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"]))                 # 2
+print(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))                 # 1
+print(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))  # 0
+print(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))# 4
+print(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+        "e",
+        "ee",
+        "eee",
+        "eeee",
+        "eeeee",
+        "eeeeee"
+        ])) # 0
