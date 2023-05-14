d96cf6c7899019ddead8219a9ae75abdc9556201
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-05-14 18:41:17 -0500

    Added my intuitive tab solution to gridTraveller problem

--- DynamicProgramming/gridTravellerFib.py
+++ DynamicProgramming/gridTravellerFib.py
@@ -1,21 +1,41 @@
 
 # time = O(m*n); space = O(m*n)
-def gridTraveller(m, n):
-    # 3 0, 1, 2, 3
-    # m = 1(2); n = 1(2)
-    tab = [[0] * (n + 1) for _ in range(m + 1)]
 
+def gridTraveller(m, n):
+
+    tab = [[0] * (n + 1) for _ in range(m + 1)]
     tab[1][1] = 1 
+
     for r in range(m + 1):
         for c in range(n + 1):
-            curr = tab[r][c]
 
-            if r + 1 <= m:
-                tab[r + 1][c] += curr 
-            if c + 1 <= n:
-                tab[r][c + 1] += curr 
+            if (r, c) != (1, 1):
+                # top is r - 1
+                if 0 <= r - 1 <= m:
+                    tab[r][c] += tab[r - 1][c]
+
+                # left is c - 1
+                if 0 <= c - 1 <= n:
+                    tab[r][c] += tab[r][c - 1]
 
     return tab[m][n]
+# ALVINS SOLUTION
+# def gridTraveller(m, n):
+#     # 3 0, 1, 2, 3
+#     # m = 1(2); n = 1(2)
+#     tab = [[0] * (n + 1) for _ in range(m + 1)]
+
+#     tab[1][1] = 1 
+#     for r in range(m + 1):
+#         for c in range(n + 1):
+#             curr = tab[r][c]
+
+#             if r + 1 <= m:
+#                 tab[r + 1][c] += curr 
+#             if c + 1 <= n:
+#                 tab[r][c + 1] += curr 
+
+#     return tab[m][n]
 
 
 
